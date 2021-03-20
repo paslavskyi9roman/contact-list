2154e0a81b434860282d401b2fb15a51dddd62d3
paslavskyi9roman <paslavsky.roman@gmail.com>
2021-03-20 19:18:25 +0200

    moving users to independent component

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,24 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import {UserService} from '../shared/user.service'
-
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
-
   constructor(public userService: UserService) { }
 
   ngOnInit(): void { 
   }
 
-  removeUser(id: number) { 
-    this.userService.removeUser(id)
-  }
 
 }
 
-// винести юзер інтерфейс з сервісу
-// розбити юзерів на окремий компонент
-// не мапати з юзерсервісу
+// ✔ винести юзер інтерфейс з сервісу
+// ❌ розбити юзера на окремий компонент
+// ✔ не мапати з юзерсервісу 
