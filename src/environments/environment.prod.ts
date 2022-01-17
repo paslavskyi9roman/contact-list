0bc1348deed2cfc94401004b01afc00d2f8bfcdc
Taras <paslavskyit@gmail.com>
2022-01-17 16:57:54 +0200

    updated files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCVCR01JbajUhT8glmZR-j-D9IYZ5gXCMA',
+    authDomain: 'contacts-ng-37a16.firebaseapp.com',
+    projectId: 'contacts-ng-37a16',
+    storageBucket: 'contacts-ng-37a16.appspot.com',
+    messagingSenderId: '207163913452',
+    appId: '1:207163913452:web:a0707ebe20cbe1125fa1c9',
+    measurementId: 'G-W3LZF0NSSF'
+
+  },
 };
